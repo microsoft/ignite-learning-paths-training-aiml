52dd1764db70628406dcd4daf0d2ff44e47661fe
Daiyu Hatakeyama <dahatake@hotmail.com>
2019-12-05 16:50:30 +0900

    Change base model to VGG19

--- aiml50/source/train.py
+++ aiml50/source/train.py
@@ -85,15 +85,15 @@ def main(run, source_path, target_path, epochs, batch, lr):
 
     # model
     info('Creating Model')
-    base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape,
-                                               include_top=False, 
-                                               weights='imagenet',
-                                               pooling='avg')
+    # base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape,
+    #                                            include_top=False, 
+    #                                            weights='imagenet',
+    #                                            pooling='avg')
 
-    #base_model = tf.keras.applications.VGG19(input_shape=img_shape,
-    #                                           include_top=False, 
-    #                                           weights='imagenet',
-    #                                           pooling='avg')
+    base_model = tf.keras.applications.VGG19(input_shape=img_shape,
+                                              include_top=False, 
+                                              weights='imagenet',
+                                              pooling='avg')
 
     #base_model = tf.keras.applications.ResNet50(input_shape=img_shape,
     #                                           include_top=False, 
